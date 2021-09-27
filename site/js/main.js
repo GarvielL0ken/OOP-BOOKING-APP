59f67dee180d35beed946b3eb6a24a08b2492e57
GarvielL0ken <garviel.loken.42@gmail.com>
2021-09-27 14:54:18 +0200

    renamed a few attributes to fit with the naming conventions. Linked some elements to the vue model, added event listeners and relevant functions

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,33 @@
 application = new Vue({
 	el : "#application",
-})
+	data : {
+		checkInDate : "",
+		checkOutDate : "",
+		numberOfDays : 1
+	},
+	methods : {
+		/*
+			Changes either the checkOutDate or numberOfDays depending on
+			which field the user changed.
+		*/
+		updateDates(state) {
+			if (state === 0)
+				this.updateCheckOutDate();
+			if (state === 1)
+				this.updateNumberOfDays();
+		},
+		updateCheckOutDate() {
+			console.log("updateCheckOutDate");
+			console.log("Number of Days: " + this.numberOfDays)
+			console.log("Check In Date: " + this.checkInDate);
+			
+			
+		},
+		updateNumberOfDays() {
+			console.log("updateNumberOfDays");
+		},
+		log() {
+			console.log("test");
+		}
+	}
+})	
