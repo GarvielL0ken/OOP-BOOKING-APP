c79aaf441d40104b580dcfd49dc9dc1dd64897f5
GarvielL0ken <garviel.loken.42@gmail.com>
2021-09-28 11:48:11 +0200

    checkOutDate and numberOfDays fields are automatically filled based on which field the user fills. Validation for check out dates that are before the check in date is in place.

--- site/js/main.js
+++ site/js/main.js
@@ -17,14 +17,38 @@ application = new Vue({
 				this.updateNumberOfDays();
 		},
 		updateCheckOutDate() {
-			console.log("updateCheckOutDate");
-			console.log("Number of Days: " + this.numberOfDays)
-			console.log("Check In Date: " + this.checkInDate);
-			
-			
+			var objCheckInDate;
+			var objCheckOutDate;
+
+			if (this.numberOfDays > 0 && this.checkInDate) {
+				objCheckInDate = new Date(this.checkInDate);
+				objCheckOutDate = new Date();
+
+				objCheckOutDate.setDate(objCheckInDate.getDate() + parseInt(this.numberOfDays)); 
+				this.checkOutDate = objCheckOutDate.toJSON();
+				this.checkOutDate = this.checkOutDate.substring(0, 10);
+			}
 		},
 		updateNumberOfDays() {
-			console.log("updateNumberOfDays");
+			const millisecondsInADay = 86400000;
+			var objCheckInDate;
+			var objCheckOutDate;
+			var intTotalNumberOfDays;
+			
+			
+			if (this.checkInDate && this.checkOutDate) {
+				objCheckInDate = new Date(this.checkInDate);
+				objCheckOutDate = new Date(this.checkOutDate);
+
+				milliseconds = objCheckOutDate.valueOf() - objCheckInDate.valueOf();
+				intTotalNumberOfDays = milliseconds / millisecondsInADay;
+				if (intTotalNumberOfDays > 0)
+					this.numberOfDays = milliseconds / millisecondsInADay;
+				else {
+					this.numberOfDays = 1;
+					this.updateCheckOutDate();
+				}
+			}
 		},
 		log() {
 			console.log("test");
