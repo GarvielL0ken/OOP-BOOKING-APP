4e984760ede6cdf151cd785fd518ec856caecc88
GarvielL0ken <garviel.loken.42@gmail.com>
2021-10-07 16:56:42 +0200

    Used XHRHandler and XHRRequsetData classes to retrieve the hotel name that the user would have set on the previous page

--- site/js/compare.js
+++ site/js/compare.js
@@ -1,10 +1,14 @@
-import {HotelInterface} from './HotelInterface.js';
+import { HotelInterface } from './HotelInterface.js';
+import { XHRHandler } from './XHRHandler.js';
+import { XHRRequestData } from './XHRRequestData.js';
 
 var application = new Vue({
 	el : "#application",
 	data : {
 		hotel1 : "",
 		hotel2 : "",
+		numberOfDays : 2,
+		xhrResponseText : "Initial Text",
 		hotels : [
 			{title : "Alpha",
 				dailyRate : 100,
@@ -15,8 +19,9 @@ var application = new Vue({
 			{title : "Charlie",
 				dailyRate : 50,
 				features : "dolar"}],
-		interface1 : new HotelInterface(),
-		interface2 : new HotelInterface()
+		interface1 : new HotelInterface(2),
+		interface2 : new HotelInterface(2),
+		xhrHandler : new XHRHandler()
 	},
 	methods : {
 		updateCurrentHotel(interfaceIndex) {
@@ -25,6 +30,27 @@ var application = new Vue({
 				this.interface1.setHotel(this.hotel1, this.hotels);
 			else
 				this.interface2.setHotel(this.hotel2, this.hotels);
+		},
+		addXHRRequestData(name, method, path) {
+			var xhrRequestData;
+
+			xhrRequestData = new XHRRequestData(name, method, path);
+			console.log(xhrRequestData);
+			this.xhrHandler.addNewXHRRequestData(xhrRequestData);
+			console.log(this.xhrHandler);
+		},
+		getHotelFromSession() {
+			var	hotelName;
+
+			hotelName = JSON.parse(this.xhrHandler.requestC());
+			console.log(hotelName);
+			this.hotel1 = hotelName;
+			this.hotel2 = hotelName;
+			this.updateCurrentHotel(1);
+			this.updateCurrentHotel(2);
 		}
 	}
 })
+
+application.addXHRRequestData('get_hotel', 'GET', '../config/get_hotel.php');
+application.getHotelFromSession();
